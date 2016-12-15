cee4651d75ae119a2cc93a8759a596b2090dfc37
YvesHe <heyuvsheyu@foxmail.com>
2016-12-15 17:07:25 +0800

    del yves.sql

--- src/cn/com/yves/servlet/AutoRefresh.java
+++ src/cn/com/yves/servlet/AutoRefresh.java
@@ -10,49 +10,51 @@ import javax.servlet.http.HttpServletResponse;
 
 public class AutoRefresh extends HttpServlet {
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 * 
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request
-	 *            the request send by the client to the server
-	 * @param response
-	 *            the response send by the server to the client
-	 * @throws ServletException
-	 *             if an error occurred
-	 * @throws IOException
-	 *             if an error occurred
-	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {// response.setIntHeader就是html中设置meta标签
-		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/xml;charset=UTF-8");
+    /**
+     * The doGet method of the servlet. <br>
+     * 
+     * This method is called when a form has its tag value method equals to get.
+     * 
+     * @param request
+     *            the request send by the client to the server
+     * @param response
+     *            the response send by the server to the client
+     * @throws ServletException
+     *             if an error occurred
+     * @throws IOException
+     *             if an error occurred
+     */
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {// response.setIntHeader就是html中设置meta标签
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/xml;charset=UTF-8");
 
-		String jsonDataStart = "{currentTime:" + "'"
-				+ new Date(System.currentTimeMillis()) + "'" + "}";
+        String jsonDataStart = "{currentTime:" + "'"
+                + new Date(System.currentTimeMillis()) + "'" + "}";
 
-		// 发送数据
-		response.getWriter().write(jsonDataStart);
-	}
+        // 发送数据
+        response.getWriter().write(jsonDataStart);
+    }
 
-	/**
-	 * The doPost method of the servlet. <br>
-	 * 
-	 * This method is called when a form has its tag value method equals to
-	 * post.
-	 * 
-	 * @param request
-	 *            the request send by the client to the server
-	 * @param response
-	 *            the response send by the server to the client
-	 * @throws ServletException
-	 *             if an error occurred
-	 * @throws IOException
-	 *             if an error occurred
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-	}
+    /**
+     * The doPost method of the servlet. <br>
+     * 
+     * This method is called when a form has its tag value method equals to
+     * post.
+     * 
+     * @param request
+     *            the request send by the client to the server
+     * @param response
+     *            the response send by the server to the client
+     * @throws ServletException
+     *             if an error occurred
+     * @throws IOException
+     *             if an error occurred
+     */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
 
 }
---
+++ src/cn/com/yves/servlet/Test.java
@@ -0,0 +1,20 @@
+/**   
+* Filename:    Test.java   
+* Copyright:   Copyright (c)2010  
+* Company:     dreams8 
+* @version:    1.0   
+* @since:  JDK 1.7
+* Create at:   2016-11-18 下午3:53:41   
+* Description:  
+*   
+* Modification History:   
+* Date    Author      Version     Description   
+* ----------------------------------------------------------------- 
+* 2016-11-18 Yves He      1.0     1.0 Version   
+*/
+package cn.com.yves.servlet;
+
+
+public class Test {
+
+}
